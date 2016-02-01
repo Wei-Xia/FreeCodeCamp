91d056051eaabb300bc2c0e8eeb77139385e5d5d
Wei Xia <xw900812@gmail.com>
2016-02-01 09:30:57 -0500

    Stand in Line

---
+++ Basic JavaScript/054.js
@@ -0,0 +1,14 @@
+function queue(arr, item) {
+  // Your code here
+  arr.push(item);
+
+  return arr.shift();  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(queue(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));

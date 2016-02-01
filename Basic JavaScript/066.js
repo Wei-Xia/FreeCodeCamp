18ff980a40512e30bddba2f11c4d553517892b15
Wei Xia <xw900812@gmail.com>
2016-02-01 10:34:30 -0500

    Comparisons with the Logical Or Operator

---
+++ Basic JavaScript/066.js
@@ -0,0 +1,13 @@
+function myTest(val) {
+  // Only change code below this line
+
+  if (val <= 9 || val >= 21) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+myTest(15);

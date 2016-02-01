22eef85f5554d98637f8f8f2aa54e02cee11a404
Wei Xia <xw900812@gmail.com>
2016-02-01 00:03:03 -0500

    Assignment with a Returned Value

---
+++ Basic JavaScript/053.js
@@ -0,0 +1,18 @@
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function process(num) {
+  return (num + 3) / 5;
+}
+
+processed = process(7);
+// Only change code below this line

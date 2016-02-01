3b4050fab2645bd96e68c75fe2082f6684da7370
Wei Xia <xw900812@gmail.com>
2016-01-31 23:59:51 -0500

    Local Scope and Functions

---
+++ Basic JavaScript/050.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  var myVar = 'use strict';
+
+
+  console.log(myVar);
+}
+myFunction();
+
+// run and check the console
+// myVar is not defined outside of myFunction
+
+//console.log(myVar);
+
+// now remove the console log line to pass the test

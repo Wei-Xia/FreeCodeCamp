ec60a9f0bb04cde6a296d8212d64ad5f0f5bc11b
Wei Xia <xw900812@gmail.com>
2016-02-02 15:42:11 -0500

    Returning Boolean Values from Functions

---
+++ Basic JavaScript/076.js
@@ -0,0 +1,14 @@
+function isLess(a, b) {
+  // Fix this code
+
+  return a < b;
+
+  // if (a < b) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+}
+
+// Change these values to test
+isLess(10, 15);

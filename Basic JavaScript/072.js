0f9e5166558dc2896c6c1c0c17e81676c4aeb793
Wei Xia <xw900812@gmail.com>
2016-02-02 15:24:54 -0500

    Selecting from many options with Switch Statements

---
+++ Basic JavaScript/072.js
@@ -0,0 +1,25 @@
+function myTest(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return answer = "alpha";
+      break;
+    case 2:
+      return answer = "beta";
+      break;
+    case 3:
+      return answer = "gamma";
+      break;
+    case 4:
+      return answer = "delta";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+myTest(1);

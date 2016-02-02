ff571636fbd189bbcbef986ea755a5c83944f3de
Wei Xia <xw900812@gmail.com>
2016-02-02 15:32:44 -0500

    Adding a default option in Switch statements

---
+++ Basic JavaScript/073.js
@@ -0,0 +1,27 @@
+function myTest(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case 'a':
+      answer = "apple";
+      break;
+    case 'b':
+      answer = "bird";
+      break;
+    case 'c':
+      answer = "cat";
+      break;
+    case 'd':
+      answer = "stuff";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+myTest(1);

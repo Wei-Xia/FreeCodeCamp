a654f803ff4e64221d4e3e91b0de42d59f23cb95
Wei Xia <xw900812@gmail.com>
2016-02-03 11:34:27 -0500

    Using Objects for Lookups

---
+++ Basic JavaScript/086.js
@@ -0,0 +1,20 @@
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    alpha : "Adams",
+    bravo : "Boston",
+    charlie : "Chicago",
+    delta : "Denver",
+    echo : "Easy",
+    foxtrot : "Frank",
+  };
+
+  // Only change code above this line
+  return lookup[val];
+}
+
+// Change this value to test
+phoneticLookup("");

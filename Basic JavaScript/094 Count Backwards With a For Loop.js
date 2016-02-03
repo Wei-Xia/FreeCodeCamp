25a498fd7312dc4b97986bf89b8d405f4d4d0ec9
Wei Xia <xw900812@gmail.com>
2016-02-03 17:28:04 -0500

    update

---
+++ Basic JavaScript/094 Count Backwards With a For Loop.js
@@ -0,0 +1,14 @@
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+for (var i = 9; i>0; i -= 2) {
+  myArray.push(i);
+}
+
+// Only change code below this line.

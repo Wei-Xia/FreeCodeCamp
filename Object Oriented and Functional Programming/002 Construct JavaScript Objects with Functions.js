21ed2f950fd01835818b35a55f3e972482693e0b
Wei Xia <xw900812@gmail.com>
2016-02-05 09:52:47 -0500

    update

---
+++ Object Oriented and Functional Programming/002 Construct JavaScript Objects with Functions.js
@@ -0,0 +1,13 @@
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 1;
+};
+
+// Only change code below this line.
+
+var MotorBike = function() {
+  this.wheels = 2;
+  this.engines = 1;
+  this.seats = 2;
+};

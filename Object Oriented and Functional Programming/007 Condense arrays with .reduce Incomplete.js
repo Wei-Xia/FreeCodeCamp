e75f4e25b2d7bf48c6a03c35945e5eab8b4ce290
Wei Xia <xw900812@gmail.com>
2016-02-05 12:29:00 -0500

    Update 007 Condense arrays with .reduce Incomplete.js

--- Object Oriented and Functional Programming/007 Condense arrays with .reduce Incomplete.js
+++ Object Oriented and Functional Programming/007 Condense arrays with .reduce Incomplete.js
@@ -1,1 +1,8 @@
+var array = [4,5,6,7,8];
+var singleVal = 0;
 
+// Only change code below this line.
+
+var singleVal = array.reduce(function(previousVal, currentVal) {
+  return previousVal + currentVal;
+}, 0);

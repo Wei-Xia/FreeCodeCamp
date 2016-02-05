5832f9a6308dac476707093c5a6d9ba7f9b822dd
Wei Xia <xw900812@gmail.com>
2016-02-05 00:14:10 -0500

    update

---
+++ Basic JavaScript/100 Generate Random Whole Numbers with JavaScript.js
@@ -0,0 +1,10 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 10);
+
+function myFunction() {
+
+  // Only change code below this line.
+
+  var a = 10 * Math.random();
+
+  return Math.floor(a);
+}

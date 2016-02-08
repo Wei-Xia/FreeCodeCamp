53c5c07a1bdc0e6efd612d6187da3c72fa702b26
Wei Xia <xw900812@gmail.com>
2016-02-08 10:13:15 -0500

    Update 003 Check for Palindromes.js

--- Front End Development Certification/Basic Algorithm Scripting/003 Check for Palindromes.js
+++ Front End Development Certification/Basic Algorithm Scripting/003 Check for Palindromes.js
@@ -5,20 +5,15 @@ function palindrome(str) {
     
   var removeSpace = lowerCase.replace(/\s/g, '');
   
-  var removeSymbol = removeSpace.replace(/[^a-zA-Z ]/g, "");
+  var removeSymbol = removeSpace.replace(/[^a-zA-Z0-9]/g, '');
     
   var finalStr = removeSymbol.split("");
   
   finalStr = finalStr.reverse();
 
-  
   finalStr = finalStr.join("");
   
-  
-  
-  return removeSymbol===finalStr;
+  return removeSymbol===finalStr;  
 }
-
-
 
 palindrome("0_0 (: /-\ :) 0-0");

ce016fbd60e12d78d376d0950a0d719ca194e85f
Wei Xia <xw900812@gmail.com>
2016-02-11 10:20:29 -0500

    Update 012 Mutations.js

---
+++ Front End Development Certification/Basic Algorithm Scripting/012 Mutations.js
@@ -0,0 +1,16 @@
+function mutation(arr) {
+  
+  arr[0]=arr[0].toLowerCase();
+  arr[1]=arr[1].toLowerCase();
+  
+  var n = arr[0].split("");
+  
+  for (i=0;i<arr[1].length;i++){
+    if(n.indexOf(arr[1][i]) === -1){
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(['hello', 'hey']);

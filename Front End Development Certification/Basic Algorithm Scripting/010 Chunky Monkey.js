4757029949ebdf40df0a80276240aac6ed6ebdf1
Wei Xia <xw900812@gmail.com>
2016-02-12 14:57:55 -0500

    Update 010 Chunky Monkey.js

--- Front End Development Certification/Basic Algorithm Scripting/010 Chunky Monkey.js
+++ Front End Development Certification/Basic Algorithm Scripting/010 Chunky Monkey.js
@@ -1,13 +1,12 @@
 function chunk(arr, size) {
   // Break it up.
-  
   var newArray = [];
   
-  for (var j = 0; j < arr.size; j++) {
-    for (var i = 0; i < size; i++) {
-      newArray[i] = arr[j];
-    }
+  for (var i = 0; i < arr.length; i = i + size) {
+    var innerArr = arr.slice(i, i + size);
+    newArray.push(innerArr);  
   }
+  
   return newArray;
 }
 
